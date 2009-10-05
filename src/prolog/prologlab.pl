c67ca50c5d88326d963e8b563ad8f419a5d529e5
Brian Gianforcaro <b.gianfo@gmail.com>
2009-10-05 13:58:56 -0400

    Project description with some method stubs

---
+++ src/prolog/prologlab.pl
@@ -0,0 +1,49 @@
+
+
+
+% Merge sort
+
+% Merge sort base cases when we have a one element list, just return it.
+int_mergesort([ONE], [ONE]).
+
+% Merge sort base case #2 when we have a empty list, return empty.
+int_mergesort([], []).
+
+split([LST], [H|T]):-
+
+merge([L1], [L2], [R] :- 
+
+int_mergesort( [HF|TF], SL ) :- 
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+epsilon([]).
+
+atom( A )
+
+seq( REG1, REG2 )
+
+alt( REG1, REG2 )
+
+star( REGEX, [L] )
+
+re_match( REGEX, ATOM, LST )
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+leaf([]).
+
+node(BTL, E, BTR)
+
+btree_to_list( BT, L )
+
+btree_depth( BT, D )
+
+btree_iso( BT1, BT2 )
+
+btree_subtree( BT1, BT2 ) 
+
+
