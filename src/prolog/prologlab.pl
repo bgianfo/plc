ea49aabf0ae661965bd3636bd921fc6cefe21714
Brian Gianforcaro <b.gianfo@gmail.com>
2009-10-08 16:32:40 -0400

    worked on some stuff

--- src/prolog/prologlab.pl
+++ src/prolog/prologlab.pl
@@ -1,44 +1,81 @@
-
 % PLC Lab #2 
 % Author: Brian Gianforcaro 
 % Tested and developed GNU Prolog v1.3.1
 
-% Merge sort routines
 
-% Split base case, can't split an empty list.
-split_two([],[],[]).
+% Clause: split_two
+% Description: Split base case, can't split an empty list.
+split_two( [], [], [] ).
+
+% Clause: split_two
+%
+% Description:
+%
 % Split a list with a single element, 
 % first is a that list, second is empty list.
-split_two([L],[L], []).
+
+split_two( [L], [L], [] ).
+
+% Clause: split_two
+%
+% Description:
+%
 % Do the actual work to split to two lists into two sections.
 % Use append() to give us two lists which would create the input list.
 % Then find the length of the two halves, If the length's differ by one or zero
 % then the list is reasonably split in half X and Y.
+
 split_two( IN, X, Y ):- append( X, Y, IN ),
                         length( X, LENX ), length( Y, LENY ), 
                         ( 1 =:= abs( LENY - LENX ) ; 0 =:= LENY - LENX ).
 
+% Clause: merge_two
+%
+% Description: 
 % Base cases, merge any empty list with a non
 % empty list by just returning the non empty.
+
 merge_two( [], L, L ).
-% Opposite base cases of the above, merge any
+
+% Clause: merge_two
+%
+% Description:
+% Reverse base cases of the above, merge any
 % empty list with a non empty list by just returning the non empty.
+
 merge_two( L, [], L ).
 
+% Clause: merge_two 
+%
+% Description:
 % If the Head of list one is less than or equal to the head of list two, 
 % recursively   merge the tail of list one with the entirety of list two. 
 merge_two( [HL1|TL1], [HL2,TL2], [HL1|MERGED] ) :- HL1 =< HL2,
                                                    merge( TL1, [HL2|TL2], MERGED ).
 
+% Clause: merge_two
+% 
+% Description:
 % If the Head of list one is greater than the head of list two, 
 % recursively  merge list one with the tail of list two. 
 merge_two( [HL1|TL1], [HL2,TL2], [HL2|MERGED] ) :- HL1 > HL2, 
                                                    merge( [HL1|TL1], TL2, MERGED ).
 
+% Clause: int_mergesort
+% 
+% Description:
 % Merge sort base case #2 when we have a empty list, return empty.
 int_mergesort( [], [] ).
+
+% Clause: int_mergesort
+%
+% Description:
 % Merge sort base cases when we have a one element list, just return it.
 int_mergesort( [ONE], [ONE] ).
+
+% Clause: int_mergesort
+%
+% Description:
 % Run the actual merge sort, LS is the input list, FL is the final sorted list.
 int_mergesort( LS, FL ) :- split( LS, L1, L2 ),
                            mergesort( L1, SORTED1 ),
@@ -64,16 +101,24 @@ re_match( REGEX, ATOM, LST ) :-
 
 leaf( [] ).
 
-node( BTL, E, BTR ) :-
+node( leaf( [] ) 
+node( leaf( [] ) , E, BTR ).
 
-btree_to_list( BT, L ) :-
+node( BTL, E, leaf( [] ) ).
 
+node( node( LL, LE, LR ), E,  node( RL, RE, RR ) ).
 
-btree_depth( BT, D ) :- 
-btree_depth( BT, D ) :- btree_depth( BT, DR ), D is DR + 1.
+btree_to_list( BT, LS ) :- node(L, E, R) =:= BT, btree_to_list( L, H ), btree_to_list( R, T ), LS is [H|T]. 
+ 
+btree_to_list( BT, LS ) :-
+
+% Detect if the recursive btree_depth is a end point, both arms are leaves.
+btree_depth( BT, N ) :- BT =:= node( leaf( [] ),X, leaf( [] ) ), N is 0.
+
+btree_depth( BT, N ) :- BT, btree_depth( BT, NR ), N is NR + 1.
 
 btree_iso( BT1, BT2 ) :-
 
 btree_subtree( BT1, BT2 ) :-
-
+  
 
