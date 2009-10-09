d19117e956540c8d272acde275c6c41ca3814160
Brian Gianforcaro <bjg1955@rit.edu>
2009-10-09 14:02:29 -0400

    Push

--- src/prolog/prologlab.pl
+++ src/prolog/prologlab.pl
@@ -1,16 +1,20 @@
+
 % PLC Lab #2 
 % Author: Brian Gianforcaro 
 % Tested and developed GNU Prolog v1.3.1
 
 
 % Clause: split_two
-% Description: Split base case, can't split an empty list.
+%
+% Description:
+% ------------
+% Split base case, can't split an empty list.
 split_two( [], [], [] ).
 
 % Clause: split_two
 %
 % Description:
-%
+% ------------
 % Split a list with a single element, 
 % first is a that list, second is empty list.
 
@@ -19,70 +23,77 @@ split_two( [L], [L], [] ).
 % Clause: split_two
 %
 % Description:
-%
+% ------------
 % Do the actual work to split to two lists into two sections.
 % Use append() to give us two lists which would create the input list.
 % Then find the length of the two halves, If the length's differ by one or zero
 % then the list is reasonably split in half X and Y.
 
-split_two( IN, X, Y ):- append( X, Y, IN ),
-                        length( X, LENX ), length( Y, LENY ), 
-                        ( 1 =:= abs( LENY - LENX ) ; 0 =:= LENY - LENX ).
+split_two( [IN], [X], [Y] ):- append( X, Y, IN ),
+                              length( X, LENX ), length( Y, LENY ),
+                              ( 1 =:= abs( LENY - LENX ) ; 0 =:= LENY - LENX ).
 
 % Clause: merge_two
 %
 % Description: 
+% ------------
 % Base cases, merge any empty list with a non
 % empty list by just returning the non empty.
 
-merge_two( [], L, L ).
+merge_two( [], [L], [L] ).
 
-% Clause: merge_two
-%
 % Description:
+% ------------
 % Reverse base cases of the above, merge any
 % empty list with a non empty list by just returning the non empty.
 
-merge_two( L, [], L ).
+merge_two( [L], [], [L] ).
 
-% Clause: merge_two 
-%
 % Description:
+% ------------
 % If the Head of list one is less than or equal to the head of list two, 
 % recursively   merge the tail of list one with the entirety of list two. 
-merge_two( [HL1|TL1], [HL2,TL2], [HL1|MERGED] ) :- HL1 =< HL2,
-                                                   merge( TL1, [HL2|TL2], MERGED ).
 
-% Clause: merge_two
-% 
+merge_two( [HL1|TL1], [HL2,TL2], [HL1|MERGED] ) :- HL1 =< HL2,
+                                                   merge_two( TL1, [HL2|TL2], MERGED ).
+
 % Description:
+% ------------
 % If the Head of list one is greater than the head of list two, 
 % recursively  merge list one with the tail of list two. 
-merge_two( [HL1|TL1], [HL2,TL2], [HL2|MERGED] ) :- HL1 > HL2, 
-                                                   merge( [HL1|TL1], TL2, MERGED ).
 
-% Clause: int_mergesort
+merge_two( [HL1|TL1], [HL2,TL2], [HL2|MERGED] ) :- HL1 > HL2, 
+                                                   merge_two( [HL1|TL1], TL2, MERGED ).
+
+% Clause: int_mergesort %
+%
+% Specification:
+% --------------
 % 
 % Description:
+% ------------
 % Merge sort base case #2 when we have a empty list, return empty.
+
 int_mergesort( [], [] ).
 
-% Clause: int_mergesort
-%
 % Description:
+% ------------
 % Merge sort base cases when we have a one element list, just return it.
+
 int_mergesort( [ONE], [ONE] ).
 
-% Clause: int_mergesort
-%
 % Description:
+% ------------
 % Run the actual merge sort, LS is the input list, FL is the final sorted list.
-int_mergesort( LS, FL ) :- split( LS, L1, L2 ),
-                           mergesort( L1, SORTED1 ),
-                           mergesort( L2, SORTED2 ),
-                           merge( SORTED1, SORTED2, FL ).
+
+int_mergesort( [LS], [FL] ) :- split_two( LS, L1, L2 ),
+                               int_mergesort( L1, SORTED1 ),
+                               int_mergesort( L2, SORTED2 ),
+                               merge_two( SORTED1, SORTED2, FL ).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Regular Expression Operations
 
 epsilon([]).
 
@@ -111,11 +122,14 @@ epsilon([]).
 
 % Clause: btree_to_list
 %
+% Description:
+% ------------
 % Base case, if we have that leaf return a empty list baby!
-btree_to_list( leaf, [] ). 
 
-% Clause: btree_to_list
-%
+btree_to_list( leaf, [] ).
+
+% Description: 
+% ------------
 % Main clause to convert a Binary Tree to a list.
 % We recursively traverse the left side of a tree until we get to a leaf.
 % If we reach a leaf we append the node value to the lists all the way up
@@ -123,6 +137,7 @@ btree_to_list( leaf, [] ).
 % traversal of the entire right side of the tree is complete, we append
 % to the head element the left arm of the list. The right side is then
 % appended to the result of the previous append. This gives us the final list.
+
 btree_to_list( node( LBT, E, RBT ), FLS ) :- btree_to_list( LBT, LL ), 
                                              btree_to_list( RBT, RL ), 
                                              append( LL, [E], LLF),
@@ -130,16 +145,52 @@ btree_to_list( node( LBT, E, RBT ), FLS ) :- btree_to_list( LBT, LL ),
  
 % Clause: btree_depth
 %
+% Specification:
+% --------------
+% Satisfied when the second integer parameter corresponds to the depth of 
+% the first binary-tree parameter. A leaf binary tree has depth zero, while
+% a node binary tree has depth one greater than the maximum of the depths of
+% its immediate sub-trees.
+%
+% Description:
+% ------------
 % Detect if the recursive btree_depth is a leaf, if so the depth is 0.
+
 btree_depth( leaf, N ) :- N is 0.
 
-% Clause: btree_depth
-%
+% Description:
+% ------------
 % Traverse the left branch, traverse the right and add the total together.
+
 btree_depth( node( LBT, _, RBT ), N ) :- btree_depth( LBT, LN ),
                                          btree_depth( RBT, RN ),
                                          N is LN + RN.
 
+% Clause: btree_iso
+%
+% Specification:
+% --------------
+% Satisfied when the two binary-tree parameters are isomorphic. Two binary 
+% trees are isomorphic if one can be derived from the other by changing the
+% order of the branches. Either of the parameters may be instantiated with a
+% variable when btree_iso is used in a top-level goal.
+%
+% Description:
+% ------------
+%
 % btree_iso( BT1, BT2 ) :-
+
+% Clause: btree_subtree
+%
+% Specification: 
+% --------------
+% Satisfied when the second binary-tree parameter is a sub-tree 
+% (not necessarily an immediate sub-tree) of the first binary-tree parameter. 
+% Either of the parameters may be instantiated with a variable when 
+% btree_subtree is used in a top-level goal.
+%
+% Description:
+% ------------
+
 
 % btree_subtree( BT1, BT2 ) :-
