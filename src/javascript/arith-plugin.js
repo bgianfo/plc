7c8dea9872a36ad0f9a81ed805a1cc47af281b2f
Brian Gianforcaro <b.gianfo@gmail.com>
2009-10-22 02:05:53 -0400

    More stuff..

--- src/javascript/arith-plugin.js
+++ src/javascript/arith-plugin.js
@@ -16,14 +16,29 @@ if (!DEBUG) {
 /**
  * Arith class constructor
  */
-function Arith() 
-{
+function Arith() {
     /* An array of numbers recently entered by the user or computed. */
-    this.stack = Array();
+    this.stack = new Array();
     this.stack[0] = 0;
 
-    this.opNames = {};
-    this.opList = {};
+    /** 
+     * An array of up to four symbols for the operations. 
+     * The symbols should match the names of the properties in opList.
+     */
+    this.opNames = { "+":"addition", 
+                      "-":"subtraction",
+                      "*":"multiplicaiton",
+                      "/":"division" };
+
+    /**
+     * An object that maps the string names of the calculator's 
+     * operation buttons to the operation methods above by use
+     * of its property names and values. 
+     */
+    this.opList = { "addition": this.addition,
+                    "subtraction": this.subtraction,
+                    "multiplicaiton":this.multiplication,
+                    "division":this.division };
 
     /**
      * Duplicate the top value on the stack.
@@ -38,8 +53,7 @@ function Arith()
      *
      * @return The top of the stack, or zero.
      */
-    this.getStackTop = function() 
-    {
+    this.getStackTop = function() {
       return (this.stack.top() === undefined) ? 0 : this.stack.top();
     }
 
@@ -49,9 +63,51 @@ function Arith()
      *
      * @param v - The value to set the top of the stack to
      */
-    this.setStackTop = function( value ) 
-    {
+    this.setStackTop = function( value ) {
+       console.log( "push: " + value );
        this.stack[0] = value; 
+    }
+
+   /**
+    * Addition operator callback
+    */ 
+    this.addition = function () {
+      console.log("calling addition")
+      first = this.stack.pop();
+      second = this.stack.pop();
+      this.setStackTop( first + second );
+    }
+
+    /**
+     * Subtraction operator callback
+     */ 
+    this.subtraction = function() {
+      console.log("calling subtraction")
+      first = this.stack.pop();
+      second = this.stack.pop();
+      this.setStackTop( first - second );
+    }
+
+    /**
+     * Multiplication operator callback
+     */ 
+    this.multiplication = function () {
+      console.log("calling multiplication")
+      first = this.stack.pop();    
+      second = this.stack.pop();
+
+      this.setStackTop( first * second ); 
+    }
+
+    /**
+     * Division operator callback
+     */ 
+    this.division = function () {
+      console.log("calling division")
+      divisor = this.stack.pop();
+      dividend = this.stack.pop(); 
+      quotient = dividend/divisor;
+      this.setStackTop( quotient );
     }
 }
 
