0e8976cf139352998d84469205493029006c5303
Brian Gianforcaro <b.gianfo@gmail.com>
2009-10-22 23:30:17 -0400

    Do work, work done!

--- src/javascript/arith-plugin.js
+++ src/javascript/arith-plugin.js
@@ -3,7 +3,6 @@
  * PLC Lab #3: JavaScript
  */
 
-
 DEBUG = true;
 
 /**
@@ -17,97 +16,100 @@ if (!DEBUG) {
  * Arith class constructor
  */
 function Arith() {
-    /* An array of numbers recently entered by the user or computed. */
-    this.stack = new Array();
-    this.stack[0] = 0;
-
-    /** 
-     * An array of up to four symbols for the operations. 
-     * The symbols should match the names of the properties in opList.
-     */
-    this.opNames = { "+":"addition", 
-                      "-":"subtraction",
-                      "*":"multiplicaiton",
-                      "/":"division" };
-
     /**
-     * An object that maps the string names of the calculator's 
-     * operation buttons to the operation methods above by use
-     * of its property names and values. 
-     */
-    this.opList = { "addition": this.addition,
-                    "subtraction": this.subtraction,
-                    "multiplicaiton":this.multiplication,
-                    "division":this.division };
+   * Duplicate the top value on the stack.
+   */
+  this.enter = function() {
+    this.stack.push( this.stack[0] );
+  }
 
-    /**
-     * Duplicate the top value on the stack.
-     */
-    this.enter = function() {
-      this.stack.push( this.stack.top() );
-    }
+  /**
+   * Return the value stored in the top of the stack, or 0 if the stack is empty.
+   * The stack is unchanged.
+   *
+   * @return The top of the stack, or zero.
+   */
+  this.getStackTop = function() {
+    return (this.stack[0] === undefined) ? 0 : this.stack[0];
+  }
 
-    /**
-     * Return the value stored in the top of the stack, or 0 if the stack is empty.
-     * The stack is unchanged.
-     *
-     * @return The top of the stack, or zero.
-     */
-    this.getStackTop = function() {
-      return (this.stack.top() === undefined) ? 0 : this.stack.top();
-    }
+  /**
+   * Set the value of the top of the stack to value. 
+   * The stack size is unchanged.
+   *
+   * @param v - The value to set the top of the stack to
+   */
+  this.setStackTop = function( value ) {
+     console.log( "push: " + value );
+     this.stack[0] = value; 
+  }
 
-    /**
-     * Set the value of the top of the stack to value. 
-     * The stack size is unchanged.
-     *
-     * @param v - The value to set the top of the stack to
-     */
-    this.setStackTop = function( value ) {
-       console.log( "push: " + value );
-       this.stack[0] = value; 
-    }
+ /**
+  * Addition operator callback
+  */ 
+  Arith.prototype.addition = function() {
+    console.log("calling addition")
+    first = parseInt( this.stack.pop() );
+    second = parseInt( this.stack.pop() );
+    this.setStackTop( first + second );
+  }  
 
-   /**
-    * Addition operator callback
-    */ 
-    this.addition = function () {
-      console.log("calling addition")
-      first = this.stack.pop();
-      second = this.stack.pop();
-      this.setStackTop( first + second );
-    }
+  /**
+   * Subtraction operator callback
+   */ 
+  Arith.prototype.subtraction = function() {
+    console.log("calling subtraction")
+    first = this.stack.pop();
+    second = this.stack.pop();
+    this.setStackTop( first - second );
+  }
 
-    /**
-     * Subtraction operator callback
-     */ 
-    this.subtraction = function() {
-      console.log("calling subtraction")
-      first = this.stack.pop();
-      second = this.stack.pop();
-      this.setStackTop( first - second );
-    }
+  /**
+   * Multiplication operator callback
+   */ 
+  Arith.prototype.multiplication = function() {
+    console.log("calling multiplication")
+    first = this.stack.pop();    
+    second = this.stack.pop();
+    this.setStackTop( first * second ); 
+  }
 
-    /**
-     * Multiplication operator callback
-     */ 
-    this.multiplication = function () {
-      console.log("calling multiplication")
-      first = this.stack.pop();    
-      second = this.stack.pop();
+  /**
+   * Division operator callback
+   */ 
+  Arith.prototype.division = function() {
+    console.log("calling division")
+    divisor = this.stack.pop();
+    dividend = this.stack.pop(); 
+    quotient = dividend/divisor;
+    this.setStackTop( quotient );
+  }
 
-      this.setStackTop( first * second ); 
-    }
+  /* An array of numbers recently entered by the user or computed. */
+  this.stack = new Array();
 
-    /**
-     * Division operator callback
-     */ 
-    this.division = function () {
-      console.log("calling division")
-      divisor = this.stack.pop();
-      dividend = this.stack.pop(); 
-      quotient = dividend/divisor;
-      this.setStackTop( quotient );
-    }
+  this.stack[0] = 0;
+
+  /** 
+   * An array of up to four symbols for the operations. 
+   * The symbols should match the names of the properties in opList.
+   */
+  this.opNames = {
+    "+":"addition", 
+    "-":"subtraction",
+    "*":"multiplication",
+    "/":"division"
+  };
+
+  /**
+   * An object that maps the string names of the calculator's 
+   * operation buttons to the operation methods above by use
+   * of its property names and values. 
+   */
+  this.opList = {
+    "addition": Arith.prototype.addition,
+    "subtraction": Arith.prototype.subtraction,
+    "multiplication": Arith.prototype.multiplication,
+    "division": Arith.prototype.division 
+  };
 }
-
