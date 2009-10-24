0d162218d86cd3d6f8a12bf52c51ff1b872ec394
Brian Gianforcaro <b.gianfo@gmail.com>
2009-10-23 21:25:37 -0400

    again

--- src/javascript/arith-plugin.js
+++ src/javascript/arith-plugin.js
@@ -8,15 +8,13 @@ DEBUG = true;
 /**
  * Turn off logging statements if they become annoying.
  */
-if (!DEBUG) {
-  console.log = function() {};
-}
+if ( !DEBUG ) { console.log = function(){}; }
+
 
 /**
  * Arith class constructor
  */
 function Arith() {
-
   /**
    * Duplicate the top value on the stack.
    */
@@ -35,29 +33,29 @@ function Arith() {
   }
 
   /**
-   * Set the value of the top of the stack to value. 
+   * Set the value of the top of the stack to value.
    * The stack size is unchanged.
    *
    * @param value - The value to set the top of the stack to
    */
   this.setStackTop = function( value ) {
      console.log( "push: " + value );
-     this.stack[0] = value; 
+     this.stack[0] = value;
   }
 
   /**
    * Addition operator callback
-   */ 
+   */
   Arith.prototype.addition = function() {
     console.log("calling addition")
     first = parseInt( this.stack.pop() );
     second = parseInt( this.stack.pop() );
     this.setStackTop( first + second );
-  }  
+  }
 
   /**
    * Subtraction operator callback
-   */ 
+   */
   Arith.prototype.subtraction = function() {
     console.log("calling subtraction")
     first = this.stack.pop();
@@ -67,50 +65,51 @@ function Arith() {
 
   /**
    * Multiplication operator callback
-   */ 
+   */
   Arith.prototype.multiplication = function() {
     console.log("calling multiplication")
-    first = this.stack.pop();    
+    first = this.stack.pop();
     second = this.stack.pop();
-    this.setStackTop( first * second ); 
+    this.setStackTop( first * second );
   }
 
   /**
    * Division operator callback
-   */ 
+   */
   Arith.prototype.division = function() {
     console.log("calling division")
     divisor = this.stack.pop();
-    dividend = this.stack.pop(); 
+    dividend = this.stack.pop();
     quotient = dividend/divisor;
     this.setStackTop( quotient );
   }
 
-  /* An array of numbers recently entered by the user or computed. */
+  /* An array of numbers recently entered by the user or computed */
   this.stack = new Array();
 
+  /* Set the top of the stack with the value zero initially */
   this.stack[0] = 0;
 
-  /** 
-   * An array of up to four symbols for the operations. 
+  /**
+   * An array of up to four symbols for the operations.
    * The symbols should match the names of the properties in opList.
    */
   this.opNames = {
-    "+":"addition", 
+    "+":"addition",
     "-":"subtraction",
     "*":"multiplication",
     "/":"division"
   };
 
   /**
-   * An object that maps the string names of the calculator's 
+   * An object that maps the string names of the calculator's
    * operation buttons to the operation methods above by use
-   * of its property names and values. 
+   * of its property names and values.
    */
   this.opList = {
     "addition": Arith.prototype.addition,
     "subtraction": Arith.prototype.subtraction,
     "multiplication": Arith.prototype.multiplication,
-    "division": Arith.prototype.division 
+    "division": Arith.prototype.division
   };
 }
