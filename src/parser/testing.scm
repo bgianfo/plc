6e578a1c16d6135f515fb6142d3f7a97ecc0459c
Brian Gianforcaro <b.gianfo@gmail.com>
2009-11-12 23:36:04 -0500

    filter tests and add some printf debugs

--- src/parser/testing.scm
+++ src/parser/testing.scm
@@ -350,31 +350,31 @@
  test09
  test10
  test11
- test12
+ ;test12
  test13
  test14
  test15
- test16
- test17
- test18
- test19
- test20
- test21
- test22
- test23
- test24
- test25
+ ;test16
+ ;test17
+ ;test18
+ ;test19
+ ;test20
+ ;test21
+ ;test22
+ ;test23
+ ;test24
+ ;test25
  test26
- test27
- test28
- test29
- test30
- test31
- test32
- test33
+ ;test27
+ ;test28
+ ;test29
+ ;test30
+ ;test31
+ ;test32
+ ;test33
  test34
  test35
- test98
+ ;test98
  test99
 ))
 
